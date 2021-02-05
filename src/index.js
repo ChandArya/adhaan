1adf50f426ab20549171bd9e35c1fb32bdcd3fba
Chandan <Chandan.kumar@createbytes.com>
2021-02-05 16:53:49 +0530

    allchang

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import Main from "./Entryfile/Main";
 
 ReactDOM.render(<Main/>, document.getElementById('app'));
 
-if (module.hot) { // enables hot module replacement if plugin is installed
- module.hot.accept();
-}
+// if (module.hot) { // enables hot module replacement if plugin is installed
+//  module.hot.accept();
+// }
